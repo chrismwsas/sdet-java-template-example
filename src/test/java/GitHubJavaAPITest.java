491077328220f2c693f5c5285cb43363b88246e0
mtlcwe <chris.m.west@sas.com>
2021-02-19 17:03:38 +0000

    stripped out user details

--- src/test/java/GitHubJavaAPITest.java
+++ src/test/java/GitHubJavaAPITest.java
@@ -11,18 +11,17 @@ public class GitHubJavaAPITest {
     @Test
     public void test_BasicAuthenticationGetUserName()
     {
+        String username = "user";
+        String oauthtoken = "token";
+
         GitHubClient client = new GitHubClient();
-        client.setCredentials("chrismwsas", "5413a523970a57e521bc489c0ff350fe15f7d7eb");
+        client.setCredentials(username, oauthtoken);
 
         UserService userService = new UserService(client);
 
         try
         {
             User user = userService.getUser();
-            String name = user.getLogin();
-
-            Assert.assertEquals("chrismwest", name);
-
         }
         catch (IOException io)
         {
